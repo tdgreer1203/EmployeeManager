3f0d9bfff59aa90f9c0461637ba857f5835991a5
Theo <tdgreer1203@gmail.com>
2022-09-01 04:12:47 -0400

    Roles API Completed

--- routes/roleRoutes.js
+++ routes/roleRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const db = require('../db/connections');
 
 router.get('/roles', (req, res) => {
-    const sql = `SELECT roles.*, departments.name AS department
-    FROM roles LEFT JOIN departments ON roles.department_id = department.id`;
+    const sql = `SELECT roles.id, roles.title, departments.name AS department, roles.salary
+    FROM roles LEFT JOIN departments ON roles.departments_id = departments.id`;
     db.query(sql, (err, rows) => {
         if(err) {
             res.status(500).json({ error: err.message });
@@ -15,8 +15,8 @@ router.get('/roles', (req, res) => {
 });
 
 router.post('/role', ({ body }, res) => {
-    const sql = `INSERT INTO roles (title, salary, department_id VALUES (?,?,?)`;
-    const params =  [body.title, body.salary, body.department_id];
+    const sql = `INSERT INTO roles (title, salary, departments_id) VALUES (?,?,?)`;
+    const params =  [body.title, body.salary, body.departments_id];
     db.query(sql, params, (err, result) => {
         if(err) {
             res.status(400).json({ error: err.message });
